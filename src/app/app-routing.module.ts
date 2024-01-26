f97b35c651e49b9f6e44962c4cc1c99e9655eab0
Murilo Cavalcanti <murilo@omotor.com.br>
2024-01-26 15:47:19 -0300

    Criação das pages

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/pages/home/home.component";
+import {SobreComponent} from "./components/pages/sobre/sobre.component";
+import {AddFoodComponent} from "./components/pages/add-food/add-food.component";
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'sobre', component: SobreComponent},
+  {path: 'foods/nova', component: AddFoodComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SobreComponent } from './components/pages/sobre/sobre.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AddFoodComponent } from './components/pages/add-food/add-food.component';
+import { FormsComponent } from './components/forms/forms.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SobreComponent,
+    HomeComponent,
+    AddFoodComponent,
+    FormsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
