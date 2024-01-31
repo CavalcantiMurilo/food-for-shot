527069174329ea14f1bab3996780d6be879b3e1c
Murilo Cavalcanti <murilo@omotor.com.br>
2024-01-31 16:31:37 -0300

    Adição de comentários

--- src/app/components/pages/food/food.component.ts
+++ src/app/components/pages/food/food.component.ts
@@ -1,8 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {Router, ActivatedRoute} from "@angular/router";
+import {FormGroup, FormControl, Validators, FormGroupDirective} from "@angular/forms";
+
 import {FoodService} from "../../../services/food.service";
 import {Food} from "../../../Food";
 import {MessagesService} from "../../../services/messages.service";
+import {Comment} from "../../../Comment";
+import {CommentService} from "../../../services/comment.service";
+
 import {environment} from "../../../../environments/environment";
 import {faTimes, faEdit} from "@fortawesome/free-solid-svg-icons";
 
@@ -19,13 +24,29 @@ export class FoodComponent implements OnInit{
   faTimes = faTimes;
   faEdit = faEdit
 
-  constructor(private foodService: FoodService, private route: ActivatedRoute, private messagesService: MessagesService, private router: Router) {
+  commentForm!: FormGroup;
+
+  constructor(private foodService: FoodService, private route: ActivatedRoute, private messagesService: MessagesService, private router: Router, private commentService: CommentService) {
   }
 
   ngOnInit(){
     const id = Number(this.route.snapshot.paramMap.get('id'));
 
-    this.foodService.getThisFood(id).subscribe((item) => (this.food = item.data))
+    this.foodService.getThisFood(id).subscribe((item) => (this.food = item.data));
+
+    this.commentForm = new FormGroup({
+      text: new FormControl("", [Validators.required]),
+      username: new FormControl("", [Validators.required]),
+    })
+
+  }
+
+  get text(){
+    return this.commentForm.get('text')!;
+  }
+
+  get username(){
+    return this.commentForm.get('username')!;
   }
 
   async removeHandler(id:number){
@@ -34,6 +55,26 @@ export class FoodComponent implements OnInit{
     this.messagesService.add("Prato excluído com sucesso!")
 
     this.router.navigate(['/']);
+  }
+
+  async onSubmit(formDirective: FormGroupDirective){
+    if (this.commentForm.invalid){
+      return;
+    }
+
+    const data: Comment = this.commentForm.value;
+
+    data.foodId = Number(this.food!.id);
+
+    await this.commentService.createComment(data).subscribe((comment) => this.food!.comments!.push(comment.data));
+
+    this.messagesService.add("Comentário adicionado!");
+
+    this.commentForm.reset();
+
+    formDirective.resetForm();
+
+
   }
 
 
